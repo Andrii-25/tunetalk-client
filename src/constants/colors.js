36f145502428c95cd65611180d4958b3b55fd9f6
Andrii <kotsupyr.a@gmail.com>
2023-03-03 21:52:28 +0200

    add main colors

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const SPACE_CADET = "2B2D42";
+export const CADET_GRAY = "8D98A7";
+export const CELADON = "B0E298";
+export const IMPERIAL_RED = "FF4242";
+export const ASPARAGUS = "7FB069";
